3f05276e8d684018bc2042b9cda3410d4748e3a9
jakub <jakub.szczucinski@epitech.eu>
2025-11-16 19:17:05 +0100

    Adding incubator bonus

--- scripts/class/player_statistics.gd
+++ scripts/class/player_statistics.gd
@@ -2,7 +2,6 @@ extends Node
 
 var time : float = 0.0
 var rot : int = 0
-var incubator : bool = false
 
 func ready() -> void:
 	_reset()
@@ -10,4 +9,3 @@ func ready() -> void:
 func _reset() -> void:
 	time = 0.0
 	rot = 0
-	incubator = false
--- scripts/game.gd
+++ scripts/game.gd
@@ -12,6 +12,10 @@ var heat : int :
 		heat = clamp(value, -20, 100)
 var heat_timer : float = 0.0
 
+var incubator : bool = false
+var is_incubate : bool = true
+var incubator_rot : float = 1.35
+
 var rot_timer : float
 var game_over : bool = false
 
@@ -33,6 +37,8 @@ func _reset() -> void:
 	_reset_humidity_timer()
 	_reset_heat_timer()
 	game_over = false
+	incubator = true
+	is_incubate = false
 
 func _reset_humidity_timer() -> void:
 	humidity_timer = 0.0
@@ -40,8 +46,18 @@ func _reset_humidity_timer() -> void:
 func _reset_heat_timer() -> void:
 	heat_timer = 0.0
 
+func _toggle_incubator():
+	incubator = not incubator
+
 func _process(delta: float) -> void:
 	if _is_game_over(): return
+	
+	if _get_incubator() and not is_incubate:
+		_set_rot_timer(_get_rot_timer() * incubator_rot)
+		is_incubate = true
+	elif not _get_incubator() and not is_incubate:
+		_set_rot_timer(_get_rot_timer() / incubator_rot)
+		is_incubate = true
 	
 	_humidity_timer(delta)
 	_heat_timer(delta)
@@ -64,6 +80,15 @@ func _heat_timer(delta : float) -> void:
 
 func _is_game_over() -> bool:
 	return game_over
-	
+
+func _get_incubator() -> bool:
+	return incubator
+
+func _set_incubator(value : bool) -> void:
+	incubator = value
+
 func _get_rot_timer() -> float:
 	return rot_timer
+	
+func _set_rot_timer(value : float) -> void:
+	rot_timer = value
