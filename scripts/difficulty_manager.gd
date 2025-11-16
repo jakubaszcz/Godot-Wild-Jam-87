694b6abaf9026237be5cdd075cb7b6918a9860bc
jakub <jakub.szczucinski@epitech.eu>
2025-11-16 11:15:08 +0100

    Setting humidity and heat

--- scripts/difficulty_manager.gd
+++ scripts/difficulty_manager.gd
@@ -6,13 +6,25 @@ var current_difficulty : Difficulty = Difficulty.Easy
 
 var data := {
 	Difficulty.Easy : {
-		"rot_timer": 3.0
+		"rot_timer": 3.0,
+		"humidity_gap": 15,
+		"heat_gap": 15,
+		"humidity_timer": 3.0,
+		"heat_timer": 2.0
 	},
 	Difficulty.Medium : {
-		"rot_timer": 2.0
+		"rot_timer": 2.0,
+		"humidity_gap": 15,
+		"heat_gap": 15,
+		"humidity_timer": 3.0,
+		"heat_timer": 2.0
 	},
 	Difficulty.Hard : {
-		"rot_timer": 1.5
+		"rot_timer": 1.5,
+		"humidity_gap": 15,
+		"heat_gap": 15,
+		"humidity_timer": 3.0,
+		"heat_timer": 2.0
 	}
 }
 
--- scripts/game.gd
+++ scripts/game.gd
@@ -4,11 +4,13 @@ extends Node
 var humidity : int :
 	set(value):
 		humidity = clamp(value, 0, 100)
+var humidity_timer : float = 0.0
 
 # Heat between -20 & 100°
 var heat : int :
 	set(value):
 		heat = clamp(value, -20, 100)
+var heat_timer : float = 0.0
 
 var rot_timer : float
 var game_over : bool = false
@@ -19,7 +21,34 @@ func _ready() -> void:
 func _reset() -> void:
 	DifficultyManager._set_difficulty(DifficultyManager.Difficulty.Easy)
 	rot_timer = DifficultyManager._get_value("rot_timer")
+	_reset_humidity_timer()
+	_reset_heat_timer()
 	game_over = false
+
+func _reset_humidity_timer() -> void:
+	humidity_timer = 0.0
+
+func _reset_heat_timer() -> void:
+	heat_timer = 0.0
+
+func _process(delta: float) -> void:
+	if _is_game_over(): return
+	
+	_humidity_timer(delta)
+	_heat_timer(delta)
+
+func _humidity_timer(delta : float) -> void:
+	humidity_timer += delta
+	if humidity_timer >= DifficultyManager._get_value("humidity_timer"):
+		humidity += 1
+		_reset_humidity_timer()
+
+func _heat_timer(delta : float) -> void:
+	heat_timer += delta
+	if heat_timer >= DifficultyManager._get_value("heat_timer"):
+		heat += 1
+		_reset_heat_timer()
+
 
 func _is_game_over() -> bool:
 	return game_over
