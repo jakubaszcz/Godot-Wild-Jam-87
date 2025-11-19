4df8cb3be09a8b9b13efd1801354eac951c80e81
jakub <jakub.szczucinski@epitech.eu>
2025-11-19 19:24:57 +0100

    Adding button holding humidity

--- scripts/interfaces/humidity/humidity_evaporer.gd
+++ scripts/interfaces/humidity/humidity_evaporer.gd
@@ -1,5 +1,26 @@
 extends Button
 
-func _on_button_down() -> void:
-	if Game._get_power_cut(): return
+var is_pressed := false
+var hold_cooldown := 0.2
+var current_cooldown := 0.0
+
+func _ready() -> void:
+	connect("button_down", Callable(self, "_on_down"))
+	connect("button_up", Callable(self, "_on_up"))
+
+func _on_down():
+	is_pressed = true
+
+func _on_up():
+	is_pressed = false
+	current_cooldown = 0.0
+
+func _process(delta: float) -> void:
+	if is_pressed:
+		current_cooldown -= delta
+		if current_cooldown <= 0.0:
+			current_cooldown = hold_cooldown
+			_on_hold()
+
+func _on_hold():
 	Signals.emit_signal("humidty", Game._get_humidity() - 1)
