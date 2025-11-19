7771e386e6d2b15060767c2c4fe11f55046941a6
jakub <jakub.szczucinski@epitech.eu>
2025-11-19 20:00:22 +0100

    Adding menu script

---
+++ scripts/menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@export var dropdown : OptionButton
+@export var button : Button
+
+var difficulties = {}
+
+func _ready() -> void:
+	difficulties = {
+		"easy": DifficultyManager.Difficulty.Easy,
+		"medium": DifficultyManager.Difficulty.Medium,
+		"hard": DifficultyManager.Difficulty.Hard,
+	}
+
+	for difficulty_name in difficulties:
+		dropdown.add_item(difficulty_name)
+
+	
+	dropdown.connect("item_selected", Callable(self, "_on_item_selected"))
+	button.connect("pressed", Callable(self, "_on_pressed"))
+
+func _on_item_selected(index) -> void:
+	var key := dropdown.get_item_text(index)
+	DifficultyManager._set_difficulty(difficulties[key])
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://nodes/game.tscn")
