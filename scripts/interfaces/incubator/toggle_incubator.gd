3ff626eacf0d48538168d66a752136de94547709
jakub <jakub.szczucinski@epitech.eu>
2025-11-21 11:36:23 +0100

    Adding new textures for incubator button toggler

--- scripts/interfaces/incubator/toggle_incubator.gd
+++ scripts/interfaces/incubator/toggle_incubator.gd
@@ -1,5 +1,12 @@
-extends Button
+extends TextureButton
+
+@export var on_texture : Texture2D
+@export var off_texture : Texture2D
 
 func _on_pressed() -> void:
 	var value = not Game.incubator
+	if value:
+		self.texture_normal = on_texture
+	else:
+		self.texture_normal = off_texture
 	Signals.emit_signal("incubate", value)
